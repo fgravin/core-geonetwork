d00b5861873fc218903be40340171777b4f5b069
Florent gravin <florent.gravin@camptocamp.com>
2015-09-08 17:00:53 +0200

    formatter-6

--- schemas/iso19139/src/main/plugin/iso19139/formatter/foss4g/view.groovy
+++ schemas/iso19139/src/main/plugin/iso19139/formatter/foss4g/view.groovy
@@ -9,8 +9,8 @@ handlers.end {
 handlers.add select: {el -> !el.'gco:CharacterString'.text().isEmpty()},
         group: true, {els ->
     def elements = els.collect {el ->
-        [name: el.name(), text: el.text()]
+        [name: f.nodeLabel(el), text: el.text()]
     }
     handlers.fileResult('foss4g/elem.html',
-            [elements: elements, parent: els[0].parent().name()])
+            [elements: elements, parent: f.nodeLabel(els[0].parent())])
 }

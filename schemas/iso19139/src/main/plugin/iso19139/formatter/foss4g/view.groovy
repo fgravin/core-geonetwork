d85ec49217e033ad292b11e270680d85f3a06a0a
Florent gravin <florent.gravin@camptocamp.com>
2015-09-08 16:43:07 +0200

    formatter-4

--- schemas/iso19139/src/main/plugin/iso19139/formatter/foss4g/view.groovy
+++ schemas/iso19139/src/main/plugin/iso19139/formatter/foss4g/view.groovy
@@ -1,4 +1,12 @@
+handlers.start {
+    '''<link rel="stylesheet" href="../../static/formatter.css"/>
+      <div class="container">'''
+}
+handlers.end {
+    '</div>'
+}
+
 handlers.add select: {el -> !el.'gco:CharacterString'.text().isEmpty()}, {el ->
-    handlers.fileResult('demo/elem.html', [name: el.name(), text: el.text()])
+    handlers.fileResult('foss4g/elem.html', [name: el.name(), text: el.text()])
 }
 
